f2bd672d6bf0889be8d48872103efdd70f26fb4a
ekole <musakazem440@gmail.com>
2017-07-14 15:49:30 +0600

    update sideNav

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 
-	function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
+if($(window).width() > 720)
+{
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+	function openNav() {
+	    document.getElementById("mySidenav").style.width = "250px";
+	}
+
+	function closeNav() {
+	    document.getElementById("mySidenav").style.width = "0";
+	}
+
+	
+} else {
+
+		function openNav() {
+		document.getElementById("mySidenav").style.width = "100%";
+	}
+
+	function closeNav() {
+		document.getElementById("mySidenav").style.width = "0";
+	}
+
 }
