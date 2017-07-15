ded067b4c79e3f0efeec372f8bceb76a34c0d361
ekole <musakazem440@gmail.com>
2017-07-16 00:54:19 +0600

    bug fix

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 
-if($(window).width() > 720)
-{
 
-	function openNav() {
-	    document.getElementById("mySidenav").style.width = "250px";
-	}
 
-	function closeNav() {
-	    document.getElementById("mySidenav").style.width = "0";
-	}
-
-	
-}
-	
-} else {
+		if($(window).width() > 720)
+	{
 
 		function openNav() {
-		document.getElementById("mySidenav").style.width = "100%";
+		    document.getElementById("mySidenav").style.width = "250px";
+		}
+
+		function closeNav() {
+		    document.getElementById("mySidenav").style.width = "0";
+		}
+
+		
+	}
+		
+		 else {
+
+			function openNav() {
+			document.getElementById("mySidenav").style.width = "100%";
+		}
+
+		function closeNav() {
+			document.getElementById("mySidenav").style.width = "0";
+		}
+
 	}
 
-	function closeNav() {
-		document.getElementById("mySidenav").style.width = "0";
-	}
 
-}
 
 
 
